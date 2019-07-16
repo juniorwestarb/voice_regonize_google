266f6f05b24cb8503d55dcc4a4d5b9cce6207d49
Junior Westarb <junior.westarb@gmail.com>
2019-07-16 01:20:38 -0300

    #1 Versionado no Git

---
+++ voice_regonize.py
@@ -0,0 +1,33 @@
+import speech_recognition as spech
+from gtts import gTTS
+from playsound import playsound
+
+def cria_audio(audio):
+	create = gTTS(audio,lang='pt-br')
+	create.save('audios/reconhecimento.mp3')
+	print("Você disse isso?")
+	playsound('audios/reconhecimento.mp3')
+
+
+def ouvir():
+	microfone = spech.Recognizer()
+	with spech.Microphone() as source:
+		#Reduz ruído
+		microfone.adjust_for_ambient_noise(source)
+		print("Diga alguma coisa: ")
+		#Armazena o audio
+		audio = microfone.listen(source)
+
+
+	try:
+		#Passa o audio para o speech_recognition para aplicação de reconhecimento de padrões
+		frase = microfone.recognize_google(audio,language='pt-BR')
+		print("Você quiz dizer: " + frase)
+
+	except spech.UnkownValueError:
+		print("Não entendi")
+
+	return reconhecimento
+
+reconhecimento = ouvir()
+cria_audio(reconhecimento)
